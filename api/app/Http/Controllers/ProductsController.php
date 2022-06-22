8f45081cb0c471e06dab0ede4dab84a5d9c6279a
Zevcore <kontakt.zevstudio@gmail.com>
2022-06-22 19:44:52 +0200

    Product endpoints

--- api/app/Http/Controllers/ProductsController.php
+++ api/app/Http/Controllers/ProductsController.php
@@ -2,9 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductsCollection;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProductsController extends Controller
 {
+    public function index(): Response
+    {
+        return response(Product::all(), 200);
+    }
 
+    public function store(Request $request): Response
+    {
+
+        $product = Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'images' => json_encode(json_decode($request->images)),
+            'stock' => $request->stock,
+            'specifications' => json_encode(json_decode($request->specifications)),
+            'basePrice' => $request->basePrice,
+            'discountPercent' => $request->discountPercent,
+            'manufacturer' => $request->manufacturer
+        ]);
+
+        return response($product, 200);
+    }
+
+    public function show($id): Response
+    {
+        $product = Product::findOrFail($id);
+
+        return response(new ProductsCollection(($product)), 200);
+
+    }
+
+    public function update(Request $request, $id): Response
+    {
+        $product = Product::findOrFail($id);
+
+        $product = Product::update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'images' => json_encode(json_decode($request->images)),
+            'stock' => $request->stock,
+            'specifications' => json_encode(json_decode($request->specifications)),
+            'basePrice' => $request->basePrice,
+            'discountPercent' => $request->discountPercent,
+            'manufacturer' => $request->manufacturer
+        ]);
+        $product->save();
+
+        return response($product, 200);
+    }
+
+    public function destroy($id): Response
+    {
+        $product = Product::findOrFail($id);
+        return response($product->delete(), 200);
+    }
 }
