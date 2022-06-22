b22667442d7527a2ef61fb72e349970b0aa5f164
Zevcore <kontakt.zevstudio@gmail.com>
2022-06-22 20:24:15 +0200

    Seeder & validation fix

--- api/app/Http/Controllers/ProductsController.php
+++ api/app/Http/Controllers/ProductsController.php
@@ -17,19 +17,21 @@ class ProductsController extends Controller
 
     public function store(StoreProductRequest $request): Response
     {
+        if($request->validated()) {
+            $product = Product::create([
+                'name' => $request->name,
+                'description' => $request->description,
+                'images' => json_encode(json_decode($request->images)),
+                'stock' => $request->stock,
+                'specifications' => json_encode(json_decode($request->specifications)),
+                'basePrice' => $request->basePrice,
+                'discountPercent' => $request->discountPercent,
+                'manufacturer' => $request->manufacturer
+            ]);
 
-        $product = Product::create([
-            'name' => $request->name,
-            'description' => $request->description,
-            'images' => json_encode(json_decode($request->images)),
-            'stock' => $request->stock,
-            'specifications' => json_encode(json_decode($request->specifications)),
-            'basePrice' => $request->basePrice,
-            'discountPercent' => $request->discountPercent,
-            'manufacturer' => $request->manufacturer
-        ]);
-
-        return response($product, 200);
+            return response($product, 200);
+        }
+        return response("Validation error", 400);
     }
 
     public function show($id): Response
@@ -42,21 +44,25 @@ class ProductsController extends Controller
 
     public function update(StoreProductRequest $request, $id): Response
     {
-        $product = Product::findOrFail($id);
+        if($request->validated()) {
+            $product = Product::findOrFail($id);
+            $product->update([
+                'name' => $request->name,
+                'description' => $request->description,
+                'images' => json_encode(json_decode($request->images)),
+                'stock' => $request->stock,
+                'specifications' => json_encode(json_decode($request->specifications)),
+                'basePrice' => $request->basePrice,
+                'discountPercent' => $request->discountPercent,
+                'manufacturer' => $request->manufacturer
+            ]);
 
-        $product = Product::update([
-            'name' => $request->name,
-            'description' => $request->description,
-            'images' => json_encode(json_decode($request->images)),
-            'stock' => $request->stock,
-            'specifications' => json_encode(json_decode($request->specifications)),
-            'basePrice' => $request->basePrice,
-            'discountPercent' => $request->discountPercent,
-            'manufacturer' => $request->manufacturer
-        ]);
-        $product->save();
+            $product->save();
 
-        return response($product, 200);
+            return response($product, 200);
+        }
+
+        return response("Validation error", 400);
     }
 
     public function destroy($id): Response
--- api/database/seeders/ProductSeeder.php
+++ api/database/seeders/ProductSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -14,6 +16,30 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $images = [
+            0 => [
+                "id" => 1,
+                "src" => "http://test.test/test.png",
+                "alt" => "Alternative text",
+                "active" => "true",
+            ]
+        ];
+
+        $specifications = [
+            "Lorem ipsum",
+            "Ipsum lorem",
+        ];
+
+
+        DB::table("products")->insert([
+            'name' => Str::random(10),
+            'description' => Str::random(10),
+            'images' => json_encode($images),
+            'stock' => 5,
+            'specifications' => json_encode($specifications),
+            'basePrice' => 21.37,
+            'discountPercent' => 10,
+            'manufacturer' => Str::random(15)
+        ]);
     }
 }
